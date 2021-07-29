cd9912be7411399c9483ccd3b4265f2a415190f7
Carlos Eduardo Pereira De Sousa Silva <carlos.silva@maximatech.com.br>
2021-07-29 20:39:18 -0300

    Create Project

---
+++ design-patterns/src/br/upis/Triangulo.java
@@ -0,0 +1,77 @@
+package br.upis;
+
+public class Triangulo {
+
+    private int ladoA;
+    private int ladoB;
+    private int ladoC;
+
+    public Triangulo() {
+        this.ladoA = 1;
+        this.ladoB = 1;
+        this.ladoC = 1;
+    }
+
+    public Triangulo(int ladoA, int ladoB, int ladoC) {
+        this();
+        if (condicaoExistencia(ladoA,ladoB,ladoC)){
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+    }
+
+    private boolean condicaoExistencia(int a, int b, int c) {
+        return Math.abs(b - c) < a && a < b + c;
+    }
+
+    private int semiPerimeter(int a, int b, int c){
+        return (a + b + c) / 2;
+    }
+
+    public double calculateArea(){
+        int s = semiPerimeter(getLadoA(), getLadoB(),getLadoC());
+        double area = s *(s - getLadoA())*(s -getLadoB())*(s -getLadoC());
+        return Math.sqrt(area);
+    }
+
+    public int getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(int ladoA) {
+        if (condicaoExistencia(ladoA, getLadoB(), getLadoC())) {
+            this.ladoA = ladoA;
+        }
+    }
+
+    public int getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(int ladoB) {
+        if (condicaoExistencia(getLadoA(), ladoB, getLadoC())) {
+            this.ladoB = ladoB;
+        }
+    }
+
+    public int getLadoC() {
+        return ladoC;
+    }
+
+    public void setLadoC(int ladoC) {
+        if (condicaoExistencia(ladoA, ladoB, ladoC)) {
+            this.ladoC = ladoC;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo{" +
+                "ladoA=" + ladoA +
+                ", ladoB=" + ladoB +
+                ", ladoC=" + ladoC +
+                '}';
+    }
+}
+
